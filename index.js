9f1559e482e6d1fdb4b5ab39cd95c331fa04e5d5
elisabethearley <elisabethearley@gmail.com>
2019-11-09 02:26:57 -0500

    started on Person class, got a few tests to pass

--- index.js
+++ index.js
@@ -41,7 +41,17 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 
+  eat(someFood) {
+    if (this.stomach.length <= 10) {
+      this.stomach.push(someFood);
+    }
+  }
 }
 
 /*
